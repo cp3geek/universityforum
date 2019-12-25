047c8d5be829f681db447bcb7ac05d70110a8fbb
cp3 <792716300@qq.com>
2019-12-25 21:14:28 +0800

    注册功能完成

--- src/main/java/com/cqgcxy/universityforum/controller/UserController.java
+++ src/main/java/com/cqgcxy/universityforum/controller/UserController.java
@@ -36,15 +36,27 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public int signUp(String user){
-        //bug
-        //曲线救国，传变量
+    public int signUp(String userName,String userPassword,
+                      String userShow,String userEmail,
+                      String userPhone,String userSex)
+    {
 
-        System.out.println(user);
+        User user=new User();
+        user.setUserImg("default.jpg");
+        user.setUserEmail(userEmail);
+        user.setUserName(userName);
+        user.setUserPassword(userPassword);
+        user.setUserPhone(userPhone);
+        user.setUserSex(userSex);
+        user.setUserShow(userShow);
+        user.setUserBlog("myself");
         Date date = new Date();//获得系统时间
+        user.setUserTime(date);
 
-//        User newUser=userService.userRegister(user);
-//        if( newUser==null)return 404;
+
+
+        User newUser=userService.userRegister(user);
+        if( newUser==null)return 404;
         return 200;
     }
 
--- src/main/java/com/cqgcxy/universityforum/domain/User.java
+++ src/main/java/com/cqgcxy/universityforum/domain/User.java
@@ -1,10 +1,12 @@
 package com.cqgcxy.universityforum.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,6 +30,8 @@ public class User {
     private String userSex;
     private String userPhone;
     private Byte userStatus;//用户状态1：激活 0：未激活
+
+
     private Date userTime;//注册时间
     private String userShow;//用户个性签名
     private String userBlog;//用户主页
