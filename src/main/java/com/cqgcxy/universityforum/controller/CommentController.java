dc08c4cbb099fb753b02fab6875c82eda840dacc
cp3 <792716300@qq.com>
2020-01-03 08:37:28 +0800

    发表评论

--- src/main/java/com/cqgcxy/universityforum/controller/CommentController.java
+++ src/main/java/com/cqgcxy/universityforum/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.cqgcxy.universityforum.controller;
 
+import com.cqgcxy.universityforum.domain.Comment;
 import com.cqgcxy.universityforum.resultmap.ViewComAndUser;
 import com.cqgcxy.universityforum.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class CommentController {
@@ -17,16 +20,30 @@ public class CommentController {
 
 
     @PostMapping("/getComment")
-    Page<ViewComAndUser>findComment
-            (
+    Page<ViewComAndUser>findComment(
                     @RequestParam(value = "page",defaultValue = "0")Integer page,
                     @RequestParam(value = "size",defaultValue = "30")Integer size,
-                    @RequestParam(value = "artId")Long artId
-            )
+                    @RequestParam(value = "artId")Long artId)
     {
-
-
         return commentService.findViewComAndUser(page,size,artId);
+    }
 
+
+    @PostMapping("/postcomment")
+    int postComment(@RequestParam(value = "comArtId")Long comArtId,
+                    @RequestParam(value = "text")String text,
+                    @RequestParam(value = "comUserId")Long comUserId)
+    {
+        Comment comment=new Comment();
+
+        Date date=new Date();
+        comment.setComArtId(comArtId);
+        comment.setComContent(text);
+        comment.setComUserId(comUserId);
+        comment.setComTime(date);
+        System.out.println(comment);
+        //添加评论，写dao层增加方法，明日更新
+
+        return 200;
     }
 }
